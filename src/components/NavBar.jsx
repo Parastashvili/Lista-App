39ec030f7558914d8aa9e24b67774a8710aff832
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-07-27 11:57:07 +0400

    navbar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { styled } from "styled-components";
 import siteLogo from "../../public/vite.svg";
 import Button from "./Button";
-
+import { Divide as Hamburger } from "hamburger-react";
 export default function NavBar() {
   const [navBG, setNavBG] = useState("transparent");
   const [navColor, setNavColor] = useState("#ffffff");
@@ -25,16 +25,33 @@ export default function NavBar() {
       window.removeEventListener("scroll", handleScrollEvent);
     };
   }, []);
+  const [isOpen, setOpen] = useState(false);
   return (
     <Main style={{ backgroundColor: `${navBG}` }}>
       <Logo src={siteLogo} />
       <Navigation>
-        <p style={{ color: `${navColor}` }}>Home</p>
-        <p style={{ color: `${navColor}` }}>Menu</p>
-        <p style={{ color: `${navColor}` }}>Locations</p>
-        <p style={{ color: `${navColor}` }}>Review</p>
-        <p style={{ color: `${navColor}` }}>About Us</p>
+        <div className="navbar">
+          <p style={{ color: `${navColor}` }}>Home</p>
+          <p style={{ color: `${navColor}` }}>Menu</p>
+          <p style={{ color: `${navColor}` }}>Locations</p>
+          <p style={{ color: `${navColor}` }}>Review</p>
+          <p style={{ color: `${navColor}` }}>About Us</p>
+        </div>
         <Button text="Contact Us" padding="7px 15px" textSize="20px" />
+        <div className="hamburger">
+          <Hamburger
+            toggled={isOpen}
+            toggle={setOpen}
+            color="#ffffff"
+            onToggle={(toggled) => {
+              if (toggled) {
+                // open a menu
+              } else {
+                // close a menu
+              }
+            }}
+          />
+        </div>
       </Navigation>
     </Main>
   );
@@ -48,7 +65,7 @@ const Main = styled.div`
   justify-content: space-between;
   padding: 10px 45px;
   @media only screen and (max-width: 600px) {
-    padding: 17px 22px;
+    padding: 15px 22px;
     width: calc(100% - 44px);
   }
 `;
@@ -57,6 +74,21 @@ const Logo = styled.img`
   height: 40px;
 `;
 const Navigation = styled.div`
+  .hamburger {
+    display: none;
+    @media only screen and (max-width: 786px) {
+      display: block;
+    }
+  }
+  .navbar {
+    display: flex;
+    align-items: center;
+    justify-content: right;
+    gap: 30px;
+    @media only screen and (max-width: 786px) {
+      display: none;
+    }
+  }
   display: flex;
   align-items: center;
   justify-content: right;
